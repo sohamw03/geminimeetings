fe4065c376e43080c2c0874c9480946e557c09bd
Soham Waghmare <102748742+sohamw03@users.noreply.github.com>
2024-04-21 01:36:04 +0530

    log: status get api added ot the backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,10 @@ app.use(cors()); // Enable CORS for all origins
 
 app.get("/api/socket", SocketHandler);
 
+app.get("/", (req, res) => {
+  res.send("GeminiMeetings WebRTC Server is running");
+});
+
 app.listen(8000, function () {
   console.log("[server]: SocketIO server listening at http://127.0.0.1:8000");
 });
